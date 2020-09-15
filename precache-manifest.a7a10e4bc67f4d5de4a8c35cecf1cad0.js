28a79ba86324242a67e42411383751e90efe5382
Alex Lee <10814755+alexlee22@users.noreply.github.com>
2020-09-15 14:37:14 +1000

    Build

---
+++ precache-manifest.a7a10e4bc67f4d5de4a8c35cecf1cad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4636bce77ca341a146b0d236da90f2e",
+    "url": "/r3f-deviceorientationcontrols/index.html"
+  },
+  {
+    "revision": "61984a2aaca1f86a15a2",
+    "url": "/r3f-deviceorientationcontrols/static/css/main.2536b54a.chunk.css"
+  },
+  {
+    "revision": "d71b717445e54a45e76f",
+    "url": "/r3f-deviceorientationcontrols/static/js/2.07655869.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/r3f-deviceorientationcontrols/static/js/2.07655869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61984a2aaca1f86a15a2",
+    "url": "/r3f-deviceorientationcontrols/static/js/main.b898f6f3.chunk.js"
+  },
+  {
+    "revision": "7726d45abab8c1cdb0c4",
+    "url": "/r3f-deviceorientationcontrols/static/js/runtime-main.173540ed.js"
+  }
+]);
